dbbff7b364355fcafd1f09c5cb317c3b11f5d39a
soarflat <soarflat777@gmail.com>
2020-02-13 22:26:06 +0900

    feat: 不要コンポーネント、不要記述を削除(#10)

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,36 +1,10 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        fanza-nuxt
-      </h1>
-      <h2 class="subtitle">
-        Fanza Nuxt
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <div class="container"></div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue';
-
 export default {
-  components: {
-    Logo
-  },
+  components: {},
   async asyncData({ $axios }) {
     const { data } = await $axios.get('/api/users');
     return { users: data };
@@ -47,36 +21,3 @@ export default {
   }
 };
 </script>
-
-<style>
-.container {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
-  margin: 0 auto;
-  text-align: center;
-}
-
-.title {
-  display: block;
-  color: #35495e;
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 100px;
-  font-weight: 300;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  padding-bottom: 15px;
-  color: #526488;
-  font-size: 42px;
-  font-weight: 300;
-  word-spacing: 5px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
